8bcdc294ab735f81544a5b4196315f4f057476b6
naveenbhuvi123 <bhuvinaveen@gmail.com>
2024-03-25 11:34:17 +0000

    authontication

---
+++ src/components/Cart/index.js
@@ -0,0 +1,18 @@
+import Header from '../Header'
+import './index.css'
+
+const Cart = () => (
+  <div>
+    <Header />
+
+    <div className="container">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-cart-img.png"
+        className="a-img"
+        alt="cart"
+      />
+    </div>
+  </div>
+)
+
+export default Cart
---
+++ src/components/Products/index.js
@@ -0,0 +1,16 @@
+import Header from '../Header'
+import './index.css'
+
+const Products = () => (
+  <div>
+    <Header />
+    <div className="container">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-products-img.png"
+        className="a-img"
+        alt="products"
+      />
+    </div>
+  </div>
+)
+export default Products
